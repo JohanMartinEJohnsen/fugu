78b1285137e7d74c050c71f7f4389a56c162b8b7
Johan Martin Johnsen <jomarn@me.com>
2021-01-23 15:30:42 +0100

    Updates

---
+++ precache-manifest.a5a334d97ee4e53ea70829624d5c6b6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b649aa9b55ce534485b8d79d4b4c8da9",
+    "url": "/fugu/index.html"
+  },
+  {
+    "revision": "e5c84c9e47578aa88a9d",
+    "url": "/fugu/static/css/main.df920ec9.chunk.css"
+  },
+  {
+    "revision": "141874a6528fe5c93c1f",
+    "url": "/fugu/static/js/2.cb926a8c.chunk.js"
+  },
+  {
+    "revision": "e5c84c9e47578aa88a9d",
+    "url": "/fugu/static/js/main.c6060f87.chunk.js"
+  },
+  {
+    "revision": "42f867ce1799025b333f",
+    "url": "/fugu/static/js/runtime~main.f5aac34c.js"
+  }
+]);
